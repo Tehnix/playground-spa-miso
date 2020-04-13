881d66bd18a18b492793e940bd2e33e84af56dc4
Christian Kjaer Laustsen <ckl@codetalk.io>
2020-04-13 14:09:11 +0200

    Fix CI build commands

--- shell-ghcjs.nix
+++ shell-ghcjs.nix
@@ -8,11 +8,14 @@ release.env.overrideAttrs (old: {
     export PATH="${miso-pkgs.pkgs.ag}/bin:$PATH"
     export PATH="${miso-pkgs.pkgs.haskell.packages.ghc865.record-dot-preprocessor}/bin:$PATH"
 
+    export PATH_TO_CABAL_OUTPUT=dist-newstyle/build/x86_64-linux/ghcjs-8.6.0.1/miso-spa-0.1.0.0/x/app-exe/build/app-exe/app-exe.jsexe
+    export PATH_TO_NIX_OUTPUT=result/bin/app-exe.jsexe
+
     # Make sure our generated .cabal file and configuration is always up-to-date.
     hpack
 
     function refresh () {
-      echo ">> Compiling index.html to file://$(pwd)/dist-newstyle/build/x86_64-linux/ghcjs-8.6.0.1/miso-spa-0.1.0.0/x/app-exe/build/app-exe/app-exe.jsexe/index.html"
+      echo ">> Compiling index.html to file://$(pwd)/$PATH_TO_CABAL_OUTPUT/index.html"
       ag -l | entr sh -c 'cabal --project-file=ghcjs.project v2-build --ghcjs'
     }
 
@@ -25,15 +28,15 @@ release.env.overrideAttrs (old: {
     }
 
     function optimize () {
-      echo "Running clouse-compiler on ./result/bin/app-exe.jsexe/all.js"
+      echo "Running clouse-compiler on ./$PATH_TO_CABAL_OUTPUT/all.js"
       mkdir -p dist
       cp ./index.html dist/index.html
 
       closure-compiler \
         --compilation_level ADVANCED_OPTIMIZATIONS \
         --jscomp_off=checkVars \
-        --externs=./result/bin/app-exe.jsexe/all.js.externs \
-        ./result/bin/app-exe.jsexe/all.js > ./dist/all.js
+        --externs=./$PATH_TO_CABAL_OUTPUT/all.js.externs \
+        ./$PATH_TO_CABAL_OUTPUT/all.js > ./dist/all.js
     }
   '';
 })
